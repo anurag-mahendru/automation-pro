f4fecf09876641be95765016bebdae1b1ea2ae7b
Anurag Mahendru <anurag.mahendru@glassbeam.com>
2014-07-21 17:41:54 +0530

    Build.scala generation and project structure generator

---
+++ project/build.scala
@@ -0,0 +1,31 @@
+import sbt._
+import sbt.Keys._
+import sbtassembly.Plugin._
+import AssemblyKeys._
+
+object ProjectBuild extends Build {
+
+  lazy val root = Project(
+    id = "root",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      name := "Automation-Pro",
+      organization := "code.hawk",
+      version := "1.0",
+      scalaVersion := "2.10.2",
+      resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/",
+      resolvers += "restlet" at "http://maven.restlet.org",
+      libraryDependencies ++= Seq(
+
+      "org.scalacheck" %% "scalacheck" % "1.11.4" % "test",
+      "org.scalatest" % "scalatest_2.10" % "2.2.0" % "test",
+      "com.typesafe.slick" % "slick_2.10" % "1.0.1",
+      "com.github.tototoshi" %% "slick-joda-mapper" % "0.4.1",
+      "com.h2database" % "h2" % "1.3.170",
+      "org.ini4j" % "ini4j" % "0.5.1",
+      "org.slf4j" % "slf4j-api" % "1.7.5",
+				 )
+					     )
+
+		    	  )
+}
